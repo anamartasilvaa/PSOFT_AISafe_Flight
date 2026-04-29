7cd95d96b077182b3c482ccd222b1317ae6374aa
anamartasilvaa <1230859@isep.ipp.pt>
2026-04-29 12:23:55 +0100

    estrutura spring initializr

---
+++ src/main/java/pt/isep/psoft/aisafe/AisafeApplication.java
@@ -0,0 +1,13 @@
+package pt.isep.psoft.aisafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AisafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/isep/psoft/aisafe/AisafeApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.isep.psoft.aisafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AisafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
